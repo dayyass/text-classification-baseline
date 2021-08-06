3f3075ec77730566669c2ccf72cb995a2631e849
Dani El-Ayyass <dayyass@yandex.ru>
2021-08-06 16:58:26 +0300

    separate main function

--- text_clf/__main__.py
+++ text_clf/__main__.py
@@ -1,4 +1,28 @@
-from .train import main
+from .train import train
+from .utils import get_argparse, get_config
+
+
+def main() -> int:
+    """
+    Main function to train baseline model.
+
+    :return: exit code.
+    :rtype: int
+    """
+
+    # argument parser
+    parser = get_argparse()
+    args = parser.parse_args()
+
+    # load config
+    config = get_config(args.config)
+    config["path_to_config"] = args.config
+
+    # train
+    train(config)
+
+    return 0
+
 
 if __name__ == "__main__":
     exit(main())
