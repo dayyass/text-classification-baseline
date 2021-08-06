bbecc2cd0ed52032c98dfd54236493cfbc185e73
Dani El-Ayyass <dayyass@yandex.ru>
2021-08-06 17:49:35 +0300

    update get_config function

--- text_clf/utils.py
+++ text_clf/utils.py
@@ -1,5 +1,7 @@
+import datetime
 import random
 from argparse import ArgumentParser
+from pathlib import Path
 from typing import Any, Dict
 
 import numpy as np
@@ -34,8 +36,21 @@ def get_config(path_to_config: str) -> Dict[str, Any]:
     :rtype: Dict[str, Any]
     """
 
+    now = datetime.datetime.now()
+
     with open(path_to_config, mode="r") as fp:
         config = yaml.safe_load(fp)
+
+    config["path_to_save_folder"] = (
+        Path(config["path_to_save_folder"]) / f"model_{now.date()}_{now.time()}"
+    )
+
+    config["path_to_config"] = path_to_config
+    config["path_to_save_model"] = config["path_to_save_folder"] / "model.joblib"
+    config["path_to_save_logfile"] = config["path_to_save_folder"] / "text-clf.log"
+    config["path_to_save_target_names_mapping"] = (
+        config["path_to_save_folder"] / "target_names.json"
+    )
 
     return config
 
