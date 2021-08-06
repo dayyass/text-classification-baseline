79cd24edf3bb1d75158b135341b709e35abfe7e5
Dani El-Ayyass <dayyass@yandex.ru>
2021-08-06 16:10:25 +0300

    rename main to train

--- text_clf/__init__.py
+++ text_clf/__init__.py
@@ -1,4 +1,4 @@
-from .main import main as text_clf_train
+from .train import main as text_clf_train
 
 __version__ = "0.1.0"
 __all__ = ["text_clf_train"]
--- text_clf/__main__.py
+++ text_clf/__main__.py
@@ -1,4 +1,4 @@
-from .main import main
+from .train import main
 
 if __name__ == "__main__":
     exit(main())
