959608818b0a24e3e487138df1a520f0578e34c5
Dani El-Ayyass <dayyass@yandex.ru>
2021-08-06 16:55:42 +0300

    make config required

--- text_clf/utils.py
+++ text_clf/utils.py
@@ -18,8 +18,7 @@ def get_argparse() -> ArgumentParser:
     parser.add_argument(
         "--config",
         type=str,
-        required=False,
-        default="config.yaml",
+        required=True,
         help="Path to config",
     )
 
