8443d7a297232a78958cb33d95ff4b275288be1b
Dani El-Ayyass <dayyass@yandex.ru>
2021-09-01 12:27:41 +0300

    add logreg C param

--- tests/hyperparams_for_tests.py
+++ tests/hyperparams_for_tests.py
@@ -3,6 +3,7 @@ grid_search_params = {
     "param_grid": {
         "tf-idf__max_df": [1.0, 0.9],
         "tf-idf__min_df": [1, 15],
+        "logreg__C": [1.0, 0.5],
     },
     "scoring": "f1_weighted",
     "cv": 2,
