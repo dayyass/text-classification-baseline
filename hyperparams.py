8865df6c88362f6ce40cdbae56cf608411d72977
Dani El-Ayyass <dayyass@yandex.ru>
2021-09-01 11:38:08 +0300

    change some parameters

--- hyperparams.py
+++ hyperparams.py
@@ -2,10 +2,10 @@
 grid_search_params = {
     "param_grid": {
         "tf-idf__ngram_range": [(1, 1), (1, 2)],
-        "tf-idf__max_df": [1.0, 0.95],
-        "tf-idf__min_df": [1, 5],
+        "tf-idf__max_df": [1.0, 0.9],
+        "tf-idf__min_df": [1, 15],
         "logreg__penalty": ["l1", "l2"],
-        "logreg__C": [1.0, 0.1],
+        "logreg__C": [1.0, 0.5],
     },
     "scoring": "f1_weighted",
     "cv": 3,
