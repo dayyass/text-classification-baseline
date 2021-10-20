8ea7272f55a335f15310214508870d0ed3316c80
Dani El-Ayyass <dayyass@yandex.ru>
2021-10-20 23:00:52 +0300

    fix _get_model_and_data function

--- text_clf/pr_roc_curve.py
+++ text_clf/pr_roc_curve.py
@@ -1,3 +1,4 @@
+import glob
 import os
 from typing import Tuple
 
@@ -20,7 +21,17 @@ def _get_model_and_data(
 ) -> Tuple[Pipeline, np.ndarray, np.ndarray]:
     """TODO"""
 
-    path_to_config = os.path.join(path_to_model_folder, "config.yaml")
+    path_to_model_folder_yaml_list = glob.glob("*.yaml")
+    if len(path_to_model_folder_yaml_list) == 0:
+        raise FileNotFoundError("There is no config file (with .yaml extension).")
+    elif len(path_to_model_folder_yaml_list) > 1:
+        raise FileNotFoundError(
+            "There are more then one config files (with .yaml extension)."
+        )
+
+    path_to_config = os.path.join(
+        path_to_model_folder, path_to_model_folder_yaml_list[0]
+    )
     path_to_model = os.path.join(path_to_model_folder, "model.joblib")
 
     # load config
