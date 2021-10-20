5d0daa13a6738c8ccc1febf12c7c5464e302f190
Dani El-Ayyass <dayyass@yandex.ru>
2021-10-20 23:13:57 +0300

    add binary classification exception

--- text_clf/pr_roc_curve.py
+++ text_clf/pr_roc_curve.py
@@ -1,4 +1,5 @@
 import glob
+import json
 import os
 from typing import Tuple
 
@@ -21,18 +22,29 @@ def _get_model_and_data(
 ) -> Tuple[Pipeline, np.ndarray, np.ndarray]:
     """TODO"""
 
+    # path_to_model
+    path_to_model = os.path.join(path_to_model_folder, "model.joblib")
+
+    # path_to_target_names
+    path_to_target_names = os.path.join(path_to_model_folder, "target_names.json")
+    with open(path_to_target_names, mode="r") as fp:
+        target_names = json.load(fp)
+    if len(target_names) != 2:  # not binary classification error
+        raise Exception(
+            f"The model must have 2 classes, but has {len(target_names)} classes."
+        )
+
+    # path_to_config
     path_to_model_folder_yaml_list = glob.glob("*.yaml")
-    if len(path_to_model_folder_yaml_list) == 0:
+    if len(path_to_model_folder_yaml_list) == 0:  # no config error
         raise FileNotFoundError("There is no config file (with .yaml extension).")
-    elif len(path_to_model_folder_yaml_list) > 1:
+    elif len(path_to_model_folder_yaml_list) > 1:  # more then 1 config error
         raise FileNotFoundError(
             "There are more then one config files (with .yaml extension)."
         )
-
     path_to_config = os.path.join(
         path_to_model_folder, path_to_model_folder_yaml_list[0]
     )
-    path_to_model = os.path.join(path_to_model_folder, "model.joblib")
 
     # load config
     config = get_config(path_to_config)
